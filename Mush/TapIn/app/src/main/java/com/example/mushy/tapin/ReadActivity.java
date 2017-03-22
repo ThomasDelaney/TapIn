041c6ec0e91323a812ad251a7c07a8409c29803e
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-03-22 22:15:38 +0000

    NFC detection added +Other stuff
    Now both Set and Read intent check if the phone has NFC and if it's
    turned on
    Both intents will detect wether there is or not a tag nearby

--- Mush/TapIn/app/src/main/java/com/example/mushy/tapin/ReadActivity.java
+++ Mush/TapIn/app/src/main/java/com/example/mushy/tapin/ReadActivity.java
@@ -1,37 +1,75 @@
 package com.example.mushy.tapin;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+public class ReadActivity extends AppCompatActivity
+{
 
-public class ReadActivity extends AppCompatActivity {
+    NfcAdapter nfcAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
 
 
         // variable that identifies the NFC
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         /*
             Test to see if the phone has NFC and if it's enabled
             Also if we have the permission to use the NFC
         */
-
-        if( nfcAdapter != null && nfcAdapter.isEnabled() )
+        if( nfcAdapter == null || !(nfcAdapter.isEnabled()) )
         {
-            Toast.makeText(this, "NFC Enabled", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            finish();
             Toast.makeText(this, "NFC Not Enabled", Toast.LENGTH_LONG).show();
+            finish();
         }
-
     }
+    protected void onNewIntent(Intent intent) {
+
+        Toast.makeText(this, "NFc Tag Found!", Toast.LENGTH_LONG).show();
+
+        super.onNewIntent(intent);
+    }
+
+    /*
+        This makes the NFC search for the nearby tags
+     */
+    @Override
+    protected void onResume()
+    {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilter = new IntentFilter[] {};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+
+        super.onResume();
+    }
+
+    /*
+        When the NFC pauses (AKA stops looking for a nearby tag)
+        We display a toast saying that it found one
+     */
+    @Override
+    protected void onPause()
+    {
+
+        nfcAdapter.disableForegroundDispatch(this);
+        onNewIntent(getIntent());
+        super.onPause();
+    }
+
 
 }
