a66ce92026c58fe2f2c38b5137faf12069c0b4f4
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-04-11 20:09:22 +0100

    New way to determine the date added

--- Mush/EmployeeLogin/app/src/main/java/com/example/mushy/employeelogin/secondActivity.java
+++ Mush/EmployeeLogin/app/src/main/java/com/example/mushy/employeelogin/secondActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
@@ -19,7 +23,7 @@ public class secondActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        setTitle("Schedule for week 1");
         BackgroundTask backgroundTask = new BackgroundTask(
         new BackgroundTask.AsyncResponse()
         {
@@ -44,63 +48,31 @@ public class secondActivity extends Activity
 
                     String[] splitInfo = day.split(",");
 
-                    System.out.println(splitInfo[0] + " " + splitInfo[1] + " " + splitInfo[2]);
-                    String day_name;
+                    System.out.println(splitInfo[0] + " " + splitInfo[1] + " " + splitInfo[2] + " " + splitInfo[3]);
+                    SimpleDateFormat newDateFormat= new SimpleDateFormat("dd/MM/yyyy");
+                    Date myDate=null;
+                    Calendar c = Calendar.getInstance(); // used to get the year
 
-                    switch(Integer.valueOf(splitInfo[0]) % 7)
+                    try
                     {
-                        case 1:
-                        {
-                            day_name="Monday";
-                            break;
-                        }
-                        case 2:
-                        {
-                            day_name="Tuesday";
-                            break;
-                        }
-                        case 3:
-                        {
-                            day_name="Wednesday";
-                            break;
-                        }
-                        case 4:
-                        {
-                            day_name="Thursday";
-                            break;
-                        }
-
-                        case 5:
-                        {
-                            day_name="Friday";
-                            break;
-                        }
-                        case 6:
-                        {
-                            day_name="Saturday";
-                            break;
-                        }
-                        case 7:
-                        {
-                            day_name="Sunday";
-                            break;
-                        }
-                        default:
-                        {
-                            day_name="Ignore This";
-                        }
-
+                        myDate = newDateFormat.parse(splitInfo[0]+"/"+splitInfo[1]+"/"+c.get(Calendar.YEAR));
                     }
-                    String[] sTimeSplit = splitInfo[1].split(":");
+                    catch (ParseException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                    newDateFormat.applyPattern("EEEE"); // get format in full weekday name
+                    String nDate = newDateFormat.format(myDate);
+
+                    String[] sTimeSplit = splitInfo[2].split(":");
                     String nSTime1= sTimeSplit[0] +":"+ sTimeSplit[1];
 
-                    sTimeSplit = splitInfo[2].split(":");
+                    sTimeSplit = splitInfo[3].split(":");
                     String nSTime2= sTimeSplit[0] +":"+ sTimeSplit[1];
 
-                    mProductList.add(new Product(Integer.valueOf(splitInfo[0]), day_name, nSTime1 + " - " +nSTime2));
+                    mProductList.add(new Product(Integer.valueOf(splitInfo[0]), nDate, nSTime1 + " - " +nSTime2));
 
-                    //Employee n = new Employee(i, splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3], splitInfo[4], splitInfo[5]);
-                    //currentEmployees.add(n);
                 }
 
 
